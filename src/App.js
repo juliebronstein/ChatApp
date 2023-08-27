a570c987fce988b05febe5db3a4df85ab2048b0c
samanta580 <samaneh.azhar@gmail.com>
2023-08-27 21:35:06 +0330

    commit 4

--- src/App.js
+++ src/App.js
@@ -3,36 +3,46 @@ import Register from './pages/Register';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import { AuthContext } from './context/AuthContext';
-// import Login from './pages/Login';
-import "./style.scss";
+import './style.scss';
 import Login from './pages/Login';
-const App = ()=>{
-  const {currentUser}=useContext(AuthContext)
-  // console.log("currentUser",currentUser)
-  const ProtectedRoute=({children})=>{
-    if(!currentUser) return <Navigate to='/login'/>
-      return children; // Return the children when currentUser is truthy
- 
-  }
-    return (
-        <BrowserRouter>
+
+const App = () => {
+  const { currentUser } = useContext(AuthContext);
+
+  const ProtectedRoute = ({ children }) => {
+    if (!currentUser) {
+      return <Navigate to="/login" />;
+    } else {
+      return children;
+    }
+  };
+
+  const NonLoggedInRoute = ({ children }) => {
+    if (currentUser) {
+      return <Navigate to="/" />;
+    } else {
+      return children;
+    }
+  };
+
+  return (
+    <BrowserRouter>
       <Routes>
         <Route path="/">
-        <Route
+          <Route
             index
             element={
               <ProtectedRoute>
                 <Home />
               </ProtectedRoute>
             }
-        
           />
-          <Route path="login" element={<Login />} />
-          <Route path="register" element={<Register />} />
+          <Route path="login" element={<NonLoggedInRoute><Login /></NonLoggedInRoute>} />
+          <Route path="register" element={<NonLoggedInRoute><Register /></NonLoggedInRoute>} />
         </Route>
       </Routes>
     </BrowserRouter>
-    ) 
-}
+  );
+};
 
 export default App;
--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -5,14 +5,35 @@ import { db } from "../firebace";
 import { ChatContext } from "../context/ChatContext";
 
 const Chats = () => {
-  const [chats, setChats] = useState([]);
+  const [chats, setChats] = useState({});
   const { currentUser } = useContext(AuthContext);
   const {dispatch}  = useContext(ChatContext);
-console.log("dispatch",dispatch)
+// console.log("dispatch",dispatch)
+  // useEffect(() => {
+  //   const getChats = () => {
+  //     const unsub = onSnapshot(doc(db, "userChats", currentUser.uid), (doc) => {
+  //       setChats(doc.data());
+  //     });
+
+  //     return () => {
+  //       unsub();
+  //     };
+  //   };
+
+  //   currentUser.uid && getChats();
+  // }, [currentUser.uid]);
+
+
+
+
+
+
+
+
   useEffect(() => {
     const getChats = () => {
       const unsub = onSnapshot(doc(db, "userChats", currentUser.uid), (doc) => {
-        setChats(doc.data());
+        setChats(doc.data() || {}); // Update chats state with fetched data or an empty object
       });
 
       return () => {
@@ -20,16 +41,59 @@ console.log("dispatch",dispatch)
       };
     };
 
-    currentUser.uid && getChats();
+    if (currentUser.uid) {
+      getChats();
+    }
   }, [currentUser.uid]);
-console.log(Object.entries(chats))
+
+
+
+
+  // useEffect(() => {
+  //   const getChats = () => {
+  //     // Set up a subscription to the userChats document
+  //     const unsub = onSnapshot(doc(db, "userChats", currentUser.uid), (doc) => {
+  //       setChats(doc.data()); // Update chats state with fetched data
+  //     });
+  
+  //     return () => {
+  //       unsub(); // Unsubscribe when the component unmounts or the UID changes
+  //     };
+  //   };
+  
+  //   // Only call getChats if currentUser.uid is defined
+  //   currentUser.uid && getChats();
+  // }, [currentUser.uid]);
+
+
+
+
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     try {
+  //       const chatsSnapshot = await getDocs(collection(db, "chats"));
+  //       const chatsData = {};
+  //       chatsSnapshot.forEach((doc) => {
+  //         chatsData[doc.id] = doc.data();
+  //       });
+  //       setChats(chatsData); // Update chats state with fetched data
+  //     } catch (error) {
+  //       console.error("Error fetching chats:", error);
+  //     }
+  //   };
+
+  //   fetchData();
+  //   currentUser.uid && getChats();
+  // }, [currentUser.uid]);
+
+
   const handleSelect = (u) => {
     dispatch({ type: "CHANGE_USER", payload: u });
   };
-
+console.log(Object.entries(chats))
   return (
     <div className="chats">
-      {Object.entries(chats)?.sort((a,b)=>b[1].date - a[1].date).map((chat) => (
+      {chats&& Object?.entries(chats)?.sort((a,b)=>b[1].date - a[1].date)?.map((chat) => (
         <div
           className="userChat"
           key={chat[0]}
